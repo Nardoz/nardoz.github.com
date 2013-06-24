0e47fde1a1b178cca87e075aac57600614bcf6f6
Tom <tomasloon@gmail.com>
2013-06-24 00:50:30 -0300

    Scaffolding cleanup + projects done

--- assets/themes/nardoz/js/nardoz.js
+++ assets/themes/nardoz/js/nardoz.js
@@ -6,51 +6,6 @@ $(document).ready(function ($) {
       $('html').toggleClass('expanded');
     });
 
-
-    // Slide Toggles
-
-    $('#section3 .button').on('click', function () {
-
-      var section = $(this).parent();
-
-    section.toggle();
-      section.siblings(".slide").slideToggle('2000', "easeInQuart");
-  });
-
-  $('#section3 .read-more').on('click', function () {
-
-      var section = $(this).parent();
-
-    section.toggle();
-      section.siblings(".slide").slideToggle('2000', "easeInQuart");
-  });
-
-  $('#section4 .article-tags li').on('click', function () {
-
-      var section = $(this).parents('.span4');
-      var category = $(this).attr('data-blog');
-      var articles = section.siblings();
-
-      // Change Tab BG's
-      $(this).siblings('.current').removeClass('current');
-      $(this).addClass('current');
-
-    // Hide/Show other articles
-      section.siblings('.current').removeClass('current').hide();
-
-      $(articles).each(function (index) {
-
-        var newCategory = $(this).attr('data-blog');
-
-        if ( newCategory == category ) {
-          $(this).slideDown('1000', "easeInQuart").addClass('current');
-        }
-      });
-
-  });
-
-
-
   // Waypoints Scrolling
 
   var links = $('.navigation').find('li');
@@ -108,55 +63,25 @@ $(document).ready(function ($) {
         goToByScroll(datasection);
     });
 
-    // Snap to scroll (optional)
-
-    /*
-
-    section.waypoint(function (direction) {
-
-        var nextpos = $(this).attr('data-section');
-        var prevpos = $(this).prev().attr('data-section');
-
-        if (nextpos != 1) {
-          if (direction === 'down') {
-              htmlbody.animate({
-                scrollTop: $('.section[data-section="' + nextpos + '"]').offset().top
-            }, 750, 'easeOutQuad');
-          }
-          else {
-              htmlbody.animate({
-                scrollTop: $('.section[data-section="' + prevpos + '"]').offset().top
-            }, 750, 'easeOutQuad');
-          }
-        }
-
-
-    }, { offset: '60%' });
-
-    */
-
-
-
-
-    // Redirect external links
-
-  $("a[rel='external']").click(function(){
-    this.target = "_blank";
+  $('a[rel="projects"]').click(function (evt) {
+    goToByScroll('3');
   });
 
+  $('#projects').html(generateProjects(projects));
 
-  // Modernizr SVG backup
-
-  if(!Modernizr.svg) {
-      $('img[src*="svg"]').attr('src', function() {
-          return $(this).attr('src').replace('.svg', '.png');
-      });
-  }
-
-
-
-
-
+  $('#projects').fisotope({
+    itemSelector: '.item',
+    layoutMode: 'masonry',
+    sortBy: 'title',
+    getSortData: {
+      title: function ($elem) {
+        return $elem.find('h2').text();
+      }
+    },
+    default_facet_operator: {
+      tags:'and'
+    }
+  });
 
 
 });
